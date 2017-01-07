ef2765f277eec09a26080d4c1df4e0a393e1f297
aapiane09 <aapiane09@gmail.com>
2017-01-06 18:04:04 -0800

    Sets up edit and delete routes, displays all and single titles, styles html to inject into index.html

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -2,9 +2,50 @@ angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
 // controller function and dependency injection
-// $routeParams and $location are required for routing stuff 
+// $routeParams and $location are required for routing stuff
 //   - but you might need to add a dependency
-BooksShowController.$inject=['$routeParams', '$location'];   
-function BooksShowController($routeParams, $location) {
+BooksShowController.$inject=['$routeParams', '$location', '$http'];
+
+function BooksShowController($routeParams, $location, $http) {
   var vm = this;
-};
+  var bookId = $routeParams.id;
+  var api = 'https://super-crud.herokuapp.com/books/'
+
+  $http({
+    method: 'GET',
+    url: api + bookId
+  }).then(function successCallback(response) {
+    vm.book = response.data;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+    $location.path('/');
+  });
+
+  vm.updateBook = function(book){
+    $http({
+      method: 'PUT',
+      url: api + bookId,
+      data: {
+        title: book.title,
+        author: book.author,
+        image: book.image,
+        releaseDate: book.releaseDate
+      }
+    }).then(function successCallback(updatedBookJson) {
+      vm.book = updatedBookJson.data;
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
+
+  vm.deleteBook = function(book){
+    $http({
+      method: 'DELETE',
+      url: api + bookId
+    }).then(function successCallback(book) {
+      $location.path('/');
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
+}
